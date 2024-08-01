5c42a6f3d6c5d9bdd528daf3b8a235a8c5cd49c7
Mchlangu <wsbungela@gmail.com>
2024-08-01 10:21:12 +0200

    created user entity and helloworld controller backend

---
+++ backend/src/main/java/com/intellidev/signmein/SignmeinApplication.java
@@ -0,0 +1,13 @@
+package com.intellidev.signmein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignmeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignmeinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/intellidev/signmein/SignmeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellidev.signmein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignmeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
